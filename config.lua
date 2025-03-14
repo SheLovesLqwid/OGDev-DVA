a2ba6ce749e6c83ec0b5e9fb76bc570870a047cb
TheOGDev <jryan75707@icloud.com>
2025-03-14 16:40:24 -0500

    Update config.lua
    Updated so all you have to do is add your webhook

--- config.lua
+++ config.lua
@@ -1,1 +1,10 @@
 
+--------------------------------------
+------Created By Whit3Xlightning & @ttvwetboi & @loxyvfx. on Dsicord------
+--https://github.com/crazy-kol https://github.com/Whit3Xlightning --
+--------------------------------------
+
+Config = {
+    Webhook = ''  -- Add your webhook here!
+    restrictCommand = true
+}
