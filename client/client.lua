c83ffbc4b69cb6418ffae1227623db14e122069a
TheOGDev <jryan75707@icloud.com>
2025-03-14 16:27:13 -0500

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -1,1 +1,18 @@
+--------------------------------------
+------Created By Whit3Xlightning & @ttvwetboi & @loxyvfx. on Disocrd------
+--https://github.com/crazy-kol https://github.com/Whit3Xlightning --
+--------------------------------------
 
+RegisterNetEvent("wld:delallveh")
+AddEventHandler("wld:delallveh", function ()
+    for vehicle in EnumerateVehicles() do
+        if (not IsPedAPlayer(GetPedInVehicleSeat(vehicle, -1))) then 
+            SetVehicleHasBeenOwnedByPlayer(vehicle, false) 
+            SetEntityAsMissionEntity(vehicle, false, false) 
+            DeleteVehicle(vehicle)
+            if (DoesEntityExist(vehicle)) then 
+                DeleteVehicle(vehicle) 
+            end
+        end
+    end
+end)
